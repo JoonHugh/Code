I trained my model using CIFAR10's dataset, and used the cross entropy loss function to calculate the loss. I used Stochastic Gradient Descent to minimize the loss and increase predictions for correct classification. I achieved 55% after I trained my model as opposed to 10%. I then saved my pytorch model as a downloadable torchscript file to be able to deploy.

Pytorch/tensor.py
@@ -45,7 +45,7 @@ r2 = torch.rand(3, 2)
 #r3 = r1 + r2 # Not allowed because the shapes are not the same
 
 # different types of operations you can do with tensors:
-r = torch.rand(2,2) - 0.5 * 2 # values between -1 and 1
+r = (torch.rand(2,2) - 0.5) * 2 # values between -1 and 1
 print(r)
 
 # common mathematical operations are supported: