b4c43f81bb104268c0fce3f80b72d7f809ab2c87
JoonHugh <joonhugh2021@gmail.com>
2025-05-01 05:10:35 -0400

    Practicing operations on tensors and mm as well

--- Pytorch/Examples/00_pytorch_fundamentals.py
+++ Pytorch/Examples/00_pytorch_fundamentals.py
@@ -97,3 +97,155 @@ if DEBUG:
     print(ones.dtype)
 
 # Create a range of tensors and tensors-like
+# use torch.range()
+if DEBUG: print(torch.arange(0, 11))
+one_to_thousand = torch.arange(start=0, end=1001, step=10)
+if DEBUG: print(one_to_thousand)
+
+# Creating tensors like replicate shape of another tensor but not define what that shape might be
+hundred_zeros = torch.zeros_like(input=one_to_thousand)
+if DEBUG: print(hundred_zeros)
+
+hundred_ones = torch.ones_like(one_to_thousand)
+if DEBUG: print(hundred_ones)
+
+
+float_32_tensor = torch.tensor([3.0, 6.0, 9.0], 
+                               dtype=None, # What datatype the tensor is (e.g. float32, int16, etc.)
+                               device=None, # What device is your tensor on (Cuda vs CPU, etc.)
+                               requires_grad=False) # Whether or not to track the gradients of a tensor when it'sd going through numerical calculations
+if DEBUG:
+    print(float_32_tensor)
+    print(float_32_tensor.dtype)
+
+""" 
+Tensor datatypes is one of the 3 big issues with PyTorch and deep learning:
+1. Tensors not right datatype (int vs floating point number) - to get datatype from tensor, can use tensor.dtype
+2. Tensors not right shape (If the shape doesn't align with multiple tensors) - to get shape from tensor, can use tensor.shape
+3. Tensors not on the right device (If one tensor lives on a GPU and another tensor lives on the CPU) - to get device from a tensor, can use tensor.device
+"""
+
+float_16_tensor = float_32_tensor.type(torch.float16)
+if DEBUG:
+    print(float_16_tensor)
+    print(float_16_tensor.dtype)
+
+multiplied_tensors = float_16_tensor * float_32_tensor
+if DEBUG:
+    print(multiplied_tensors)
+    print(multiplied_tensors.dtype)
+
+
+int_32_tensor = torch.tensor([3, 6, 9], 
+                             dtype=torch.int32)
+
+if DEBUG: print(int_32_tensor)
+
+float_and_int_tensor = float_32_tensor * int_32_tensor
+if DEBUG:
+    print(float_and_int_tensor)
+    print(float_and_int_tensor.dtype)
+
+
+# Creating a new trensor
+some_tensor = torch.rand(size=(3, 4))
+print(some_tensor)
+print(f"the type of some_tensor: {some_tensor.dtype}")
+print(f"The shape of some_tensor: {some_tensor.shape}") # can also use tensor.size()
+print(f"The device of some_tensor: {some_tensor.device}")
+
+# Maninpulating tensors
+"""
+NN are comprised with a lot of mathematical operations
+Tensor operations include:
+* Addition
+* Subtraction
+* Multiplication (element-wise)
+* Division
+* Matrix Multiplication
+"""
+
+tensor = torch.tensor([1, 2, 3])
+if DEBUG:
+    print("add 10: ", tensor + 10)
+    print("add 100: ", tensor + 100)
+
+    print("multiply 10:", tensor * 10)
+    print("multiply 100:", tensor * 100)
+
+    print("subtract 10:", tensor - 10)
+
+    # Try the built in torch operations
+    print("torch mult", torch.mul(tensor, 10))
+
+# matrix multiplication
+tensor2 = torch.tensor([4, 5, 6])
+print("matrix multiplication", torch.matmul(tensor, tensor2))
+
+"""
+Two main ways of performing multiplication in NN and deep learning
+* Element-wise multiplication
+* Matrix multiplication (dot product) - possibly the most common tensor operation in NN
+"""
+
+if DEBUG:
+
+    print(tensor,  "*", tensor)
+    print(f"Equals: {tensor * tensor}")
+
+"""
+torch.matmul
+"""
+
+print(torch.matmul(tensor, tensor)) # why 14 instead of [1, 4, 9]?
+
+# matrix multiplication by hand:
+# 1 * 1 + 2 * 2 + 3 * 3 = 14
+value = 0
+for i in range(len(tensor)):
+    value += tensor[i] * tensor[i]
+if DEBUG: print("by hand:", value)
+
+# However, built in matmul function is MUCH more efficient
+
+"""
+One of the most common errors in deep learning are shape errors.
+There are two main rules that perform matrix multiplication needs to satisfy:
+1. The inner dimension must match
+(3, 2) @ (3, 2) won't work
+(2, 3) @ (3, 2) WILL work
+(3, 2) @ (2, 3) will work
+
+The resulting matrix has the shape of the outer dimensions
+"""
+
+if DEBUG: print(tensor @ tensor)
+
+rand_tensor = torch.rand(5, 10)
+rand_tensor2 = torch.rand(10, 9)
+
+if DEBUG: print(torch.matmul(rand_tensor, rand_tensor2))
+
+# Shapes for MM
+tensor_A = torch.tensor([[1, 2],
+                         [3, 4],
+                         [5, 6]])
+
+tensor_B = torch.tensor([[7, 10],
+                         [8, 11],
+                         [9, 12]])
+
+print("shape of tensor_A", tensor_A.shape)
+print("shape of tensor_B", tensor_B.shape)
+#print("tensor_A and tensor_b matmul:", torch.mm(tensor_A, tensor_B)) # torch.mm is the same as torch.matmul (it's an alais for matmul)
+
+# reshapeing the tensors
+"""
+To fix the tensor shape issue, we can manipulate the shape of one of our tensors using transpose
+
+Transpose switchs the axes or dimensions of a given tensors
+"""
+
+tensor_B = tensor_B.T
+print("NOW mm the two:", torch.mm(tensor_A, tensor_B))
+
