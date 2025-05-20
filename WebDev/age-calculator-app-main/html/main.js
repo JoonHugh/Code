8ca4b5af20f2d0041355cd4f11a77cb93a655ebe
JoonHugh <joonhugh2021@gmail.com>
2025-05-20 15:19:31 +0900

    finished the age calculator design and the calculations, but have yet to do error handling (changing colors, making sure valid inputs for days, months, years) etc. I will finish this another day. I need to work on something else

---
+++ WebDev/age-calculator-app-main/html/main.js
@@ -0,0 +1,84 @@
+DEBUG = 0;
+
+let validYear;
+
+const submit = document.querySelector('#submit')
+
+
+const form = document.querySelector('.form')
+console.log(form)
+console.log("Hello World")
+
+const date = new Date();
+
+console.log(date.getFullYear(), date.getMonth(), date.getDate());
+if (DEBUG) console.log(typeof date.getFullYear());
+
+function invalidYear() {
+    console.log("INVALID YEAR");
+    validDate = false;
+}
+
+function invalidMonth() {
+    console.log("INVALID MONTH");
+    validDate = false;
+}
+
+function invalidDay() {
+    console.log("INVALID DAY");
+    validDate = false;
+}
+
+
+submit.addEventListener('click', (e) => {
+    if (DEBUG) console.log("CLICKED");
+
+    validDate = true;
+
+    const year = document.querySelector('#inputYear');
+    const month = document.querySelector('#inputMonth');
+    const day = document.querySelector('#inputDay');
+
+    if (year.value.trim() == "") {
+        invalidYear();
+    }
+    if (month.value.trim() == "") {
+        invalidMonth();
+    }
+    if (day.value.trim() == "") {
+        invalidDay();
+    }
+
+    if (validYear) {
+        const birthDay = new Date(year.value, month.value - 1, day.value);
+
+        let resYear = date.getFullYear() - birthDay.getFullYear();
+        let resMonth = 0;
+        date.getMonth() > birthDay.getMonth() ? (resMonth = date.getMonth() - birthDay.getMonth() - 1) : (resYear--, resMonth = 12 - (birthDay.getMonth() - date.getMonth()));
+        if (DEBUG) {
+            console.log("DATE.GETMONTH()", date.getMonth());
+            console.log("BIRTHDAY.GETMONTH()", birthDay.getMonth());
+            console.log("SUBTRACTING", date.getMonth() - birthDay.getMonth());
+            console.log("MONTH", resMonth);
+            console.log("date.now", Date.now());
+            console.log("birthDay", birthDay.getTime());
+            console.log("AGE", age.getUTCFullYear() - 1970);
+            console.log("RESULT", resYear, resMonth, resDay);
+            console.log(typeof year.value);
+            console.log("year, month, day", year.value, month.value, day.value);
+        }
+
+        if (validDate) {
+            const resDay = 31 - Math.abs(date.getDate() - day.value) - 1;
+            const diff_ms = Date.now() - birthDay.getTime();
+            const age = new Date(diff_ms);
+    
+        
+        
+            document.querySelector('#year').innerHTML = `${resYear}`;
+            document.querySelector('#month').innerHTML = `${resMonth}`;
+            document.querySelector('#day').innerHTML = `${resDay}`;
+        } 
+    }
+
+});
