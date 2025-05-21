7b1e9b237ccfceceb7fe66c4ba42b557135718c0
JoonHugh <joonhugh2021@gmail.com>
2025-05-21 20:07:20 +0900

    Optimizing my code a little bit

--- WebDev/my-react-app/src/MainGrid.jsx
+++ WebDev/my-react-app/src/MainGrid.jsx
@@ -1,33 +1,37 @@
-import React, { useState } from 'react';
 
-function MainGrid({ items }) {
+import React, {useState} from 'react';
 
-    const [clickedItem, setClickedItem] = useState(null);
-    
-    const handleItemClick = (item) => {
-        setClickedItem(item);
-    };
+function MainGrid() {
+
+    const tabs = [{id: "tab1", name: "Modes & ISA"}, {id: "tab2", name: "On-Chip Memory"}, {id: "tab3", name: "MMIO Base Addr"}, 
+                  {id: "tab4", name: "Ports"}, {id: "tab5", name: "Security"}, {id: "tab6", name: "Debug & Trace"}, {id: "tab7", name: "Interrupts"}, 
+                  {id: "tab8", name: "Design For Test"}, {id: "tab9", name: "Clocks and Reset"}, {id: "tab10", name: "Power"}, 
+                  {id: "tab11", name: "Branch Prediction"}, {id: "tab12", name: "World Guard"}, {id: "tab13", name: "Other Default IPs"}, 
+                  {id: "tab14", name: "RTL Options"}
+                 ];
+
+    const tabItems = tabs.map(tab => <li key={tab.name} className="generic-styled-tab">{tab.name}</li>)
+
+    const [tab, setTab] = useState();
+
+    const handleClick = (e) => {
+        const siblings = e.target.parentElement.children;
+        e.target.parentElement.style.borderLeft = "0px solid transparent";
+        for (let el of siblings) {
+            el.style.borderLeft = "5px solid transparent";
+            el.style.color = "hsl(0, 0%, 55%)";
+        }
+        console.log(e);
+        // e.target.parentElement.children.style.borderLeft = "5px solid transparent";
+        e.target.style.borderLeft = "5px solid hsl(14, 100%, 55%)";
+        e.target.style.color = "hsl(0, 0%, 35%)";
+    }
+
 
     return(
         <div className="main-grid">
             <div className="left-column">
-                <ul className="tab-list">
-                    {}
-                        <li className="generic-styled-tab">Modes & ISA</li>
-                        <li className="generic-styled-tab">On-Chip Memory</li>
-                        <li className="generic-styled-tab">MMIO Base Addr</li>
-                        <li className="generic-styled-tab">Ports</li>
-                        <li className="generic-styled-tab">Security</li>
-                        <li className="generic-styled-tab">Debug & Trace</li>
-                        <li className="generic-styled-tab">Interrupts</li>
-                        <li className="generic-styled-tab">Design For Test</li>
-                        <li className="generic-styled-tab">Clocks and Reset</li>
-                        <li className="generic-styled-tab">Power</li>
-                        <li className="generic-styled-tab">Branch Prediction</li>
-                        <li className="generic-styled-tab">World Guard</li>
-                        <li className="generic-styled-tab">Other Default IPs</li>
-                        <li className="generic-styled-tab">RTL Options</li>
-                </ul>
+                <ul onClick={handleClick} className="tab-list">{tabItems}</ul>
             </div>
             <div className="middle-column">
                 <h2>Modes & ISA</h2>
