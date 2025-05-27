52682f3d96c5f34fcded10817d139548b76fafb6
JoonHugh <joonhugh2021@gmail.com>
2025-05-27 11:18:00 +0900

    practiced with path modules, os modules, and most importantly, crypto modules for cryptography which includes RSA

---
+++ WebDev/Node/osDemo.js
@@ -0,0 +1,17 @@
+import os from 'os';
+
+// userInfo()
+console.log(os.userInfo());
+
+// totalmem()
+console.log(os.totalmem());
+
+// freemem()
+console.log(os.freemem());
+
+//cpus()
+console.log(os.cpus());
+
+console.log(os.machine())
+console.log(os.networkInterfaces())
+console.log(os.version())
---
+++ WebDev/Node/urlDemo.js
@@ -0,0 +1,29 @@
+import url from 'url';
+
+const urlString = 'https://www.google.com/search?q=hello+world';
+
+// URL Object
+const urlObj = new URL(urlString);
+
+console.log(urlObj);
+
+// format()
+console.log(url.format(urlObj));
+
+// import.meta.url - file URL
+console.log("import.meta.url:", import.meta.url)
+
+// fileURLToPath()
+console.log("fileURLToPath:", url.fileURLToPath(import.meta.url));
+
+console.log(urlObj.search);
+
+const params = new URLSearchParams(urlObj.search);
+console.log(params);
+console.log(params.get('q'));
+
+params.append('limit', '5');
+console.log(params);
+
+params.delete('limit');
+console.log(params);
