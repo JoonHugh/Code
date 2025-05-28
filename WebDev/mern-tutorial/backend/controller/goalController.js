b47abbdee3adf42a4749e2623081ba831819aa5b
JoonHugh <joonhugh2021@gmail.com>
2025-05-28 15:26:12 +0900

    initial REST API for goals

--- WebDev/mern-tutorial/backend/controller/goalController.js
+++ WebDev/mern-tutorial/backend/controller/goalController.js
@@ -1,10 +1,16 @@
 import asyncHandler from 'express-async-handler';
 
+import Goal from '../model/goalModel.js';
+
 // @desc   Get all goals
 // @route  GET /api/goals
 // @access Private
 export const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Get goals" });
+    const goals = await Goal.find();
+
+    // res.status(200).json({ message: "Get goals" });
+
+    res.status(200).json(goals);
 });
 
 // @desc   Get single goal
@@ -22,20 +28,45 @@ export const setGoal = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error("Please add a text field");
     }
+
+    const goal = await Goal.create({ 
+        text: req.body.text
+    })
     console.log(req.body);
-    res.status(200).json({ message: "Set goals" });
+    // res.status(200).json({ message: "Set goals" });
+    res.status(200).json(goal);
+
 });
 
 // @desc   Update goal
 // @route  PUT /api/goals/:id
 // @access Private
 export const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}` });
+
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal) {
+        res.status(400)
+        throw new Error('Goal not found');
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true});
+
+    // res.status(200).json({ message: `Update goal ${req.params.id}` });
+    res.status(200).json(updatedGoal);
 });
 
 // @desc   Delete goal
 // @route  DELETE /api/goals/:id
 // @access Private
 export const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete goal ${req.params.id}` });
+    const goal = await Goal.findByIdAndDelete(req.params.id)
+
+    if (!goal) {
+        res.status(400);
+        throw new Error('Goal not found');
+    }
+
+    // res.status(200).json({ message: `Delete goal ${req.params.id}` });
+    res.status(200).json({ id: req.params.id });
 });
--- WebDev/mern-tutorial/backend/server.js
+++ WebDev/mern-tutorial/backend/server.js
@@ -1,11 +1,16 @@
 import express, { urlencoded } from 'express';
 import dotenv from 'dotenv';
-import routes from './routes/goalRoutes.js'
+import routes from './routes/goalRoutes.js';
 import errorHandler from './middleware/errorMiddleware.js';
+import colors from 'colors';
+import connectDB from './config/db.js';
+
 
 const PORT = process.env.PORT || 5000;
 
 const config = dotenv.config();
+
+connectDB();
 
 const app = express()
 
