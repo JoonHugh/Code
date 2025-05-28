e3911e57e933a2d51b2e612d1bc3e4aabc99694f
JoonHugh <joonhugh2021@gmail.com>
2025-05-28 14:23:19 +0900

    Goals controller and routes setup with error handling

--- WebDev/mern-tutorial/backend/server.js
+++ WebDev/mern-tutorial/backend/server.js
@@ -1,1 +1,19 @@
-console.log('Hello World');
+import express, { urlencoded } from 'express';
+import dotenv from 'dotenv';
+import routes from './routes/goalRoutes.js'
+import errorHandler from './middleware/errorMiddleware.js';
+
+const PORT = process.env.PORT || 5000;
+
+const config = dotenv.config();
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:false }));
+
+app.use('/api/goals/', routes);
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
